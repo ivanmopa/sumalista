847692c61a39b775e57ccc33e516b2339c18185d
Ivan Molina <ivan95cadiz@gmail.com>
2016-10-29 16:38:46 +0200

    Faltan los índices

--- li_impar.py
+++ li_impar.py
@@ -2,8 +2,8 @@
 #-*- coding: utf-8 -*-
 #Hacer una función que reciba una lista, y devuelta otra lista 
 #solamente con los elementos de indice impar.
-def impar(ele1, ele2, ele3, ele4, ele5, ele6):
-	li = [ele1, ele2, ele3, ele4, ele5, ele6]
+li = []
+def impar(li):
 	otrali=[]
 	for elemento in li:
 		if elemento != li[1] and elemento != li[3] and elemento != li[5]:
