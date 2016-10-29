e2924da76aed804886e30965710be6f0b2cbdb17
Ivan Molina <ivan95cadiz@gmail.com>
2016-10-29 04:36:49 +0200

    hardcodeao a tope

--- li_impar.py
+++ li_impar.py
@@ -2,11 +2,11 @@
 #-*- coding: utf-8 -*-
 #Hacer una función que reciba una lista, y devuelta otra lista 
 #solamente con los elementos de indice impar.
-def impar(li):
-	li = ['hola', 247, 474, 'Pakito', True, 'Belén Esteban', 'tortilla']
+def impar(ele1, ele2, ele3, ele4, ele5, ele6):
+	li = [ele1, ele2, ele3, ele4, ele5, ele6]
 	otrali=[]
 	for elemento in li:
-		if elemento != li[2][4][6]:
+		if elemento != li[1] and elemento != li[3] and elemento != li[5]:
 			otrali.append(elemento)
 		else:
 			otrali = otrali
