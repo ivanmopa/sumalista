bf4193abfb2b0fe70e1427dad8ec820b402f279b
Ivan Molina <ivan95cadiz@gmail.com>
2016-10-31 03:16:11 +0100

    Indexpower versión definitiva

--- checkio/indexpower.py
+++ checkio/indexpower.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 def index_power(li, N):
-	resultado = 0
-	for elemento in li:		
-		if li.index(elemento)==N:
-			resultado = elemento**N
-		elif N > li.index(elemento):
-			resultado = -1
+	resultado = 0		
+	if N < len(li):
+		resultado = li[N]**N
+	else:
+		resultado = -1
 	return resultado
 
-#No vale porque si tienes dos valores iguales en la lista, li.index
-#no sabe a cúal de los dos te estás refiriendo y siempre coge el
-#que tiene el índice menor porque es en el orden en el que aparecen.
 print index_power([1, 2, 3, 4], 2) #== 9
 print index_power([1, 3, 10, 100], 3) #== 1000000
 print index_power([0, 1], 0) #== 1
 print index_power([1, 2], 3) #== -1
+print index_power([1, 1, 1, 1, 1, 1, 1], 6) #Debería dar 1
