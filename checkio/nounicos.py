29445e34a52dac0ff7b14d1e984103f7fae324b7
Ivan Molina <ivan95cadiz@gmail.com>
2016-10-31 01:38:58 +0100

    Nounicos versión definitiva

--- checkio/nounicos.py
+++ checkio/nounicos.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+li = []
+def checkio(li):
+	otrali = []
+	for numero in li:
+		if li.count(numero) > 1:
+			otrali.append(numero)
+		else:
+			otrali = otrali
+	return otrali
 
-
-
-
-
-
-
-
-
-
-
-
-
-checkio([1, 2, 3, 1, 3]) #== [1, 3, 1, 3]
-checkio([1, 2, 3, 4, 5]) #== []
-checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
-checkio([10, 9, 10, 10, 9, 8]) #== [10, 9, 10, 10, 9]
-​
+print checkio([1, 2, 3, 1, 3]) #== [1, 3, 1, 3]
+print checkio([1, 2, 3, 4, 5]) #== []
+print checkio([5, 5, 5, 5, 5]) #== [5, 5, 5, 5, 5]
+print checkio([10, 9, 10, 10, 9, 8]) #== [10, 9, 10, 10, 9]
