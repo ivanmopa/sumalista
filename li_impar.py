85a879ff16386a6c275d25e5c444dede4d981303
Ivan Molina <ivan95cadiz@gmail.com>
2016-10-31 04:19:14 +0100

    Versión definitiva. Ahora sí que sí

--- li_impar.py
+++ li_impar.py
@@ -6,17 +6,20 @@ li = []
 def impar(li):
 	otrali=[]
 	for elemento in li:
-		if li.index(elemento)%2==0:
-			otrali = otrali
-		else:
-			otrali.append(elemento)
+		otrali = li[1::2]
 	return otrali
+
+#Si miras el commit anterior, verás que la estaba liando pardísima y que estaba
+#complicando el código de una manera increíble. Así está mucho más simplificado.
+
 
 print impar([32, 'pakito', 'chorizo', 1300, 233, 'tortilla']) #Debería dar ['pakito', 1300, 'tortilla']
 print impar(['hola', True, 'Jacinto', 2458, 14]) #Debería dar [True, 2458]
 print impar(['nada']) #Debería dar []
 print impar([False, 13, 45, True, 248, True, 458, 13]) #Debería dar [13, True, True, 13]
 print impar([1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1]) #Debería dar [1, 1, 1, 1, 1, 1]
-#Haciendo un ejercicio de Checkio me di cuenta de que li.index solo tenía en cuenta
-#el primer índice que encontraba cuando se repetían dos valores. Así que al añadir
-#esta última prueba, me he dado cuenta de que falla. 
+
+#Nota mental: li[1::2] significa que vaya desde la posición 1 hasta el final. Y
+#con el último :2 estoy indicando el espacio entre salto y salto. Es decir,
+#que vaya desde la posición 1 hasta el final saltando los índices pares.
+
