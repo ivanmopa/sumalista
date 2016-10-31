dad435a9898c8fecfcf6104c30a6968e8db161a9
Ivan Molina <ivan95cadiz@gmail.com>
2016-10-31 04:09:46 +0100

    Al final falla

--- li_impar.py
+++ li_impar.py
@@ -16,3 +16,7 @@ print impar([32, 'pakito', 'chorizo', 1300, 233, 'tortilla']) #Debería dar ['pa
 print impar(['hola', True, 'Jacinto', 2458, 14]) #Debería dar [True, 2458]
 print impar(['nada']) #Debería dar []
 print impar([False, 13, 45, True, 248, True, 458, 13]) #Debería dar [13, True, True, 13]
+print impar([1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1]) #Debería dar [1, 1, 1, 1, 1, 1]
+#Haciendo un ejercicio de Checkio me di cuenta de que li.index solo tenía en cuenta
+#el primer índice que encontraba cuando se repetían dos valores. Así que al añadir
+#esta última prueba, me he dado cuenta de que falla. 
